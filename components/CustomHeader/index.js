f534a988b65668b61fc90e0f37b7a309c409930f
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2018-12-16 13:41:01 +0100

    Fix back button on DoorOpener page

--- components/CustomHeader/index.js
+++ components/CustomHeader/index.js
@@ -3,9 +3,6 @@ import { StyleSheet, Text, View, Image} from 'react-native';
 import { Content, Header, Body, Icon, Title, Container } from 'native-base';
 
 export default class CustomHeader extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     if (this.props.icon != null){
       return (
@@ -17,9 +14,8 @@ export default class CustomHeader extends React.Component {
           <Icon style={styles.icon} name="md-menu" onPress={() => this.props.navigation.openDrawer() }/>
         </View>
       );
-    } else{
+    } else {
       return (
-
         <View style={styles.body} >
           <Image source={ require('../../images/logo.png') } style={styles.logo} onPress={() => this.props.navigate() } />
           <View style={styles.bodyContainer}>
@@ -27,14 +23,12 @@ export default class CustomHeader extends React.Component {
           </View>
           <Icon style={styles.icon} name="md-menu" onPress={() => this.props.navigation.openDrawer() }/>
         </View>
-
       );
     }
   }
 }
 
 const styles = StyleSheet.create({
-
   body: {
     paddingTop: 24,
     backgroundColor: '#F9A423',
--- components/DoorOpener/index.js
+++ components/DoorOpener/index.js
@@ -1,30 +1,20 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button, Image} from 'react-native';
-import {Content} from 'native-base';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import Config from '../../config';
-import {Client, Message} from 'react-native-paho-mqtt';
+import { Client, Message } from 'react-native-paho-mqtt';
 
 import CustomHeader from '../CustomHeader';
-
 
 export default class DoorOpener extends React.Component {
   static navigationOptions = {
     drawerLabel: 'Døråpner',
     drawerIcon: (
-      <Image source={ require('../../images/døråpner.png') } style={{width: 24, height: 24}}/>
+      <Image source={require('../../images/døråpner.png')} style={{ width: 24, height: 24 }}/>
     ),
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      mqttstuff: 'Heilå',
-      noeAnnet: 'yoyoyo'
-    };
-  }
-
   componentDidMount() {
-    //Set up an in-memory alternative to global localStorage
+    // Set up an in-memory alternative to global localStorage
     const myStorage = {
       setItem: (key, item) => {
         myStorage[key] = item;
@@ -36,16 +26,16 @@ export default class DoorOpener extends React.Component {
     };
 
     // Create a client instance
-    const client = new Client({uri: `wss://${Config.doorOpener.mqttbrokerUrl}:${Config.doorOpener.mqttbrokerPort}/ws`, clientId: 'clientId', storage: myStorage});
+    const client = new Client({ uri: `wss://${Config.doorOpener.mqttbrokerUrl}:${Config.doorOpener.mqttbrokerPort}/ws`, clientId: 'clientId', storage: myStorage });
 
-    // set event handlers
+    // Set event handlers
     client.on('connectionLost', (responseObject) => {
       if (responseObject.errorCode !== 0) {
-        console.log(responseObject.errorMessage);
+        console.error(responseObject.errorMessage);
       }
     });
 
-    // connect the client
+    // Connect the client
     const connectOptions = {
       userName: Config.doorOpener.userName,
       password: Config.doorOpener.password,
@@ -68,8 +58,8 @@ export default class DoorOpener extends React.Component {
 
   render() {
     return (
-      <View style={{flex: 1}}>
-        <CustomHeader title={'Døråpner'} icon={'ios-arrow-back'} navigate={() => this.props.navigation.goBack(null)}/>
+      <View style={{ flex: 1 }}>
+        <CustomHeader title={'Døråpner'} icon={'ios-arrow-back'} navigation={this.props.navigation} />
         <View style={styles.content}>
           <Text>Nå bør døren åpne seg...</Text>
         </View>
@@ -78,7 +68,7 @@ export default class DoorOpener extends React.Component {
   }
 }
 
-//STYLES
+// STYLES
 const styles = StyleSheet.create({
   content: {
     justifyContent: 'center',
