86865c7cf150c41bf0a4101bcd8fcb634b4d971f
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2018-12-16 13:13:16 +0100

    Scroll frontpage and sticky header

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableHighlight, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableHighlight, Image, ImageBackground } from 'react-native';
 import { Content } from 'native-base';
 
 import CustomHeader from '../CustomHeader';
@@ -21,74 +21,76 @@ export default class Calendar extends React.Component {
   static navigationOptions = {
     drawerLabel: 'Home',
     drawerIcon: (
-      <Image source={ require('../../images/hjem.png') } style={{width: 24, height: 24}}/>
+      <Image source={require('../../images/hjem.png')} style={{ width: 24, height: 24 }} />
     ),
   };
 
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View style={{flex: 1}}>
+      <React.Fragment>
         <CustomHeader title={"Hjem"} navigation={this.props.navigation} />
+        <ScrollView style={{ flex: 1 }}>
 
-        <View style={styles.content}>
+          <View style={styles.content}>
 
-          <ImageBackground  style={styles.largeImg} source={ require('../../images/kortspill.jpg') }>
-            <TouchableHighlight style={styles.largeLink} onPress={() => navigate('Kortspill') }>
-              <View style={styles.textBar}>
-                <Text style={{color: '#fff'}}>Kortspill</Text>
-              </View>
-            </TouchableHighlight>
-          </ImageBackground>
-
-          <View style={styles.row}>
-            <ImageBackground  style={styles.img} source={ require('../../images/vaskelister.jpg') }>
-              <TouchableHighlight style={styles.link} onPress={() => navigate('Vaskelister') }>
+            <ImageBackground style={styles.largeImg} source={require('../../images/kortspill.jpg')}>
+              <TouchableHighlight style={styles.largeLink} onPress={() => navigate('Kortspill')}>
                 <View style={styles.textBar}>
-                  <Text style={{color: '#fff'}}>Vaskelister</Text>
+                  <Text style={{ color: '#fff' }}>Kortspill</Text>
                 </View>
               </TouchableHighlight>
             </ImageBackground>
-            <ImageBackground  style={styles.img} source={ require('../../images/vaskemaskiner.jpg') }>
-              <TouchableHighlight style={styles.link} onPress={() => navigate('Klesvask') }>
-                <View style={styles.textBar}>
-                  <Text style={{color: '#fff'}}>Vaskemaskiner</Text>
-                </View>
-              </TouchableHighlight>
-            </ImageBackground>
+
+            <View style={styles.row}>
+              <ImageBackground style={styles.img} source={require('../../images/vaskelister.jpg')}>
+                <TouchableHighlight style={styles.link} onPress={() => navigate('Vaskelister')}>
+                  <View style={styles.textBar}>
+                    <Text style={{ color: '#fff' }}>Vaskelister</Text>
+                  </View>
+                </TouchableHighlight>
+              </ImageBackground>
+              <ImageBackground style={styles.img} source={require('../../images/vaskemaskiner.jpg')}>
+                <TouchableHighlight style={styles.link} onPress={() => navigate('Klesvask')}>
+                  <View style={styles.textBar}>
+                    <Text style={{ color: '#fff' }}>Vaskemaskiner</Text>
+                  </View>
+                </TouchableHighlight>
+              </ImageBackground>
+            </View>
+
+            <View style={styles.row}>
+              <ImageBackground style={styles.img} source={require('../../images/vorskalender.png')}>
+                <TouchableHighlight style={styles.link} onPress={() => navigate('Calendar')}>
+                  <View style={styles.textBar}>
+                    <Text style={{ color: '#fff' }}>Vorskalender</Text>
+                  </View>
+                </TouchableHighlight>
+              </ImageBackground>
+              <ImageBackground style={styles.img} source={require('../../images/turneringer.jpg')}>
+                <TouchableHighlight style={styles.link} onPress={() => navigate('Turneringer')}>
+                  <View style={styles.textBar}>
+                    <Text style={{ color: '#fff' }}>Turneringer</Text>
+                  </View>
+                </TouchableHighlight>
+              </ImageBackground>
+            </View>
+
+            <View style={styles.row}>
+              <ImageBackground style={styles.img} source={require('../../images/døråpner.jpg')}>
+                <TouchableHighlight style={styles.link} onPress={() => navigate('Døråpner')}>
+                  <View style={styles.textBar}>
+                    <Text style={{ color: '#fff' }}>Døråpner</Text>
+                  </View>
+                </TouchableHighlight>
+              </ImageBackground>
+              <View style={styles.img} />
+            </View>
+
           </View>
 
-          <View style={styles.row}>
-            <ImageBackground  style={styles.img} source={ require('../../images/vorskalender.png') }>
-              <TouchableHighlight style={styles.link} onPress={() => navigate('Calendar') }>
-                <View style={styles.textBar}>
-                  <Text style={{color: '#fff'}}>Vorskalender</Text>
-                </View>
-              </TouchableHighlight>
-            </ImageBackground>
-            <ImageBackground  style={styles.img} source={ require('../../images/turneringer.jpg') }>
-              <TouchableHighlight style={styles.link} onPress={() => navigate('Turneringer') }>
-                <View style={styles.textBar}>
-                  <Text style={{color: '#fff'}}>Turneringer</Text>
-                </View>
-              </TouchableHighlight>
-            </ImageBackground>
-          </View>
-
-          <View style={styles.row}>
-            <ImageBackground  style={styles.img} source={ require('../../images/døråpner.jpg') }>
-              <TouchableHighlight style={styles.link} onPress={() => navigate('Døråpner') }>
-                <View style={styles.textBar}>
-                  <Text style={{color: '#fff'}}>Døråpner</Text>
-                </View>
-              </TouchableHighlight>
-            </ImageBackground>
-            <View style={styles.img} />
-          </View>
-
-        </View>
-
-      </View>
+        </ScrollView>
+      </React.Fragment>
     );
   }
 }
